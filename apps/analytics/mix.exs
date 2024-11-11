ccc2a7ff3f55816b5075316a725aa3ecdec181b6
Zack Kayser <zkayser@gmail.com>
2024-11-10 22:34:00 -0500

    Move analytics to apps path

--- apps/analytics/mix.exs
+++ apps/analytics/mix.exs
@@ -4,6 +4,10 @@ defmodule Analytics.MixProject do
   def project do
     [
       app: :analytics,
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
--- mix.exs
+++ mix.exs
@@ -5,7 +5,8 @@ defmodule LiveTracing.MixProject do
     [
       app: :live_tracing,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.17",
+      apps_path: "apps",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
