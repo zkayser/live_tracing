c601ab218051c278a0c482d60b4a60a55266202f
Zack Kayser <zkayser@gmail.com>
2024-11-13 06:32:40 -0500

    Expand on TracedGenServer

--- apps/otp_trace_tools/lib/distributed_client.ex
+++ apps/otp_trace_tools/lib/distributed_client.ex
@@ -1,5 +1,7 @@
 defmodule DistributedClient do
-  def call(process, message, timeout \\ 5000) do
-    GenServer.call(process, {:"$__otel_ctx", OpenTelemetry.Ctx.get_current(), message})
+
+  def call(process, original_message, timeout \\ 5000) do
+    GenServer.call(process, {:"$__otel_ctx", :otel_ctx.get_current(), original_message}, timeout)
   end
+
 end
