6c0d2eb92b14c5282844e089d83a5b0943feb1a0
Zack Kayser <zkayser@gmail.com>
2024-11-13 06:28:42 -0500

    Provide client example using DistributedClient instead of GenServer

--- apps/live_tracing/lib/live_tracing/analytics_client.ex
+++ apps/live_tracing/lib/live_tracing/analytics_client.ex
@@ -1,10 +1,6 @@
 defmodule AnalyticsClient do
-  @moduledoc """
-  Barebones client to call GenServer on other node
-  """
 
   def increment_message do
-    current_context = :otel_propagator_text_map.inject([])
-    GenServer.call({:global, Analytics.Messaging.Server}, {:message_received, current_context}, 50)
+    DistributedClient.call({:global, Analytics.Messaging.Server}, :message_received, 50)
   end
 end
