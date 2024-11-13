3e3f29f914e894b52ed0ec29f62431250f6f2290
Zack Kayser <zkayser@gmail.com>
2024-11-13 06:27:27 -0500

    Add to TracedGenServer example

--- apps/analytics/lib/messaging/server.ex
+++ apps/analytics/lib/messaging/server.ex
@@ -1,6 +1,6 @@
 defmodule Analytics.Messaging.Server do
   require Record
-  use GenServer
+  use TracedGenServer
 
   require OpenTelemetry.Tracer, as: Tracer
 
@@ -16,13 +16,20 @@ defmodule Analytics.Messaging.Server do
   end
 
   @impl GenServer
-  def handle_call({:message_received, span_ctx}, _from, state) do
-    :otel_propagator_text_map.extract(span_ctx)
-
+  def handle_call(:message_received, _from, state) do
     Tracer.with_span "#{inspect(__MODULE__)}.message_received/0",
-                     %{attrs: %{"peer.service" => "analytics", kind: :server}} do
+                     %{attributes: %{:"peer.service" => "analytics"}, kind: :server} do
       new_state = Map.update!(state, :messages_received, &(&1 + 1))
       {:reply, new_state.messages_received, new_state}
+    end
+  end
+
+  @impl GenServer
+  def handle_cast({:message_received, span_ctx}, state) do
+    Tracer.with_span(span_ctx, "#{inspect(__MODULE__)}.message_received/0",
+                     %{attributes: %{:"peer.service" => "analytics"}, kind: :server}) do
+      new_state = Map.update!(state, :messages_received, &(&1 + 1))
+      {:noreply, new_state}
     end
   end
 end
